6f08a642bf3b0ae741dd615ee1b88039ab4c0911
Loc Vo <v.locvt3@vinbrain.net>
2021-01-31 23:19:51 +0700

    Improve responsive

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,32 +1,39 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { Box, makeStyles, Theme } from '@material-ui/core';
 import Post from '../Post/Post';
 import DisplayMode from '../Post/DisplayMode';
 
 interface PropsType {
   posts: Record<string, any>[]
 }
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    [theme.breakpoints.up('md')]: {
+      height: 'calc(100vh - 110px)',
+      overflow: 'auto',
+    },
+  },
+}));
 
-const PostList = ({ posts }: PropsType) => (
-  <Box style={{
-    height: 'calc(100vh - 110px)',
-    overflow: 'auto',
-  }}
-  >
-    {posts.map((post) => (
-      <Post
-        key={post.id}
-        id={post.id}
-        title={post.title}
-        content={post.content}
-        createdAt={new Date(post.createdAt)}
-        createdBy={post.createdBy}
-        description={post.description}
-        displayMode={DisplayMode.SUMMARY}
-        tags={post.tags}
-      />
-    ))}
-  </Box>
-);
+const PostList = ({ posts }: PropsType) => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.root}>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          content={post.content}
+          createdAt={new Date(post.createdAt)}
+          createdBy={post.createdBy}
+          description={post.description}
+          displayMode={DisplayMode.SUMMARY}
+          tags={post.tags}
+        />
+      ))}
+    </Box>
+  );
+};
 
 export default PostList;
